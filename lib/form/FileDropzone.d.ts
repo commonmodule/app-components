fd01048ff3732ad3661e7b8c3cbacf23a8ff9ad8
yjgaia <yjdev88@gmail.com>
2024-12-05 13:40:36 +0900

    Add FileDropzone component and update index exports

---
+++ lib/form/FileDropzone.d.ts
@@ -0,0 +1,13 @@
+import { DomNode } from "@common-module/app";
+interface FileDropzoneOptions {
+    accept: string;
+    multiple: boolean;
+    onUpload: (files: File[]) => void;
+}
+export default class FileDropzone extends DomNode {
+    private options;
+    private invisibleFileInput;
+    constructor(classNames: `.${string}`, options: FileDropzoneOptions, ...children: DomNode[]);
+}
+export {};
+//# sourceMappingURL=FileDropzone.d.ts.map
