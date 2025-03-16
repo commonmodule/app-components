0bd7e1bb2c000f1c0562b668aaf1865f8f259654
yjgaia <yjdev88@gmail.com>
2025-03-16 12:40:24 +0900

    Implement drag-and-drop functionality in MainImageViewer

--- lib/image-viewer/MainImageViewer.d.ts
+++ lib/image-viewer/MainImageViewer.d.ts
@@ -1,5 +1,14 @@
 import { DomNode } from "@common-module/app";
 export default class MainImageViewer extends DomNode {
+    private image;
+    private isDragging;
+    private dragStartX;
+    private dragStartY;
+    private translateX;
+    private translateY;
     constructor(imageUrl: string);
+    private startDrag;
+    private drag;
+    private endDrag;
 }
 //# sourceMappingURL=MainImageViewer.d.ts.map
--- src/image-viewer/MainImageViewer.ts
+++ src/image-viewer/MainImageViewer.ts
@@ -1,10 +1,43 @@
 import { DomNode, el } from "@common-module/app";
 
 export default class MainImageViewer extends DomNode {
+  private image: DomNode;
+
+  private isDragging = false;
+  private dragStartX = 0;
+  private dragStartY = 0;
+  private translateX = 0;
+  private translateY = 0;
+
   constructor(imageUrl: string) {
     super(".main-image-viewer");
     this.append(
-      el("img", { src: imageUrl }),
+      this.image = el("img", { src: imageUrl }),
     );
+
+    this.image.onDom("mousedown", (event) => this.startDrag(event));
+    this.onWindow("mousemove", (event) => this.drag(event));
+    this.onWindow("mouseup", () => this.endDrag());
+  }
+
+  private startDrag(event: MouseEvent) {
+    event.preventDefault();
+    this.isDragging = true;
+    this.dragStartX = event.clientX - this.translateX;
+    this.dragStartY = event.clientY - this.translateY;
+  }
+
+  private drag(event: MouseEvent) {
+    if (this.isDragging) {
+      this.translateX = event.clientX - this.dragStartX;
+      this.translateY = event.clientY - this.dragStartY;
+      this.image.style({
+        transform: `translate(${this.translateX}px, ${this.translateY}px)`,
+      });
+    }
+  }
+
+  private endDrag() {
+    this.isDragging = false;
   }
 }
